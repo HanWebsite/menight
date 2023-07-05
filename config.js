2990aa59387a15fecc90289886b12ab6a054622a
Han-DasBoard <123283630+HanWebsite@users.noreply.github.com>
2023-07-05 14:28:45 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+let option = {
+    server_name: "MeNight-Ps",
+    server_description: "The Best Private  Server",
+    owner: [
+        "Dafa",
+        "NightMeowGT"
+    ],
+    discord_invite_link: "https://discord.gg/MynYrVWcuv",
+    ip_address: "139.180.187.222 Dan 138.180.219.69",
+    host_for_android: "https://www.mediafire.com/file/chgdsq5mxlvp49u/MeNight-PS.host/file"
+}
+
